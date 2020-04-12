27c9bf6f18ad2cffc1fd9ec9105578ba39ac588a
Mot101 <56612863+Mot101@users.noreply.github.com>
2020-04-12 18:58:46 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+int dp[MAX][MAX];
+int main(){
+    int n,m;
+    cin >> n >> m;
+    dp[1][1]=1;
+    for (int i=2;i<=n;i++)
+    for (int j=2;j<=m;j++)
+    dp[i][j]=dp[i-1][j-2]+dp[i-2][j-1];
+    cout << dp[n][m];
+return 0;
+}
