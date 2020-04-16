d60c04321440c9dc280eabf773dec54fdc4474b1
Mot101 <56612863+Mot101@users.noreply.github.com>
2020-04-16 12:12:49 +0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long n,s,e,emax,summ=0,summmax=0;
+    cin >> n;
+    vector <int> a(100000000);
+    for (int i=0;i<n;i++){
+        cin >> s >> e;
+        a[s]+=1;
+        a[e]-=1;
+    }
+    for (long long i=0;i<10000000;i++){
+        summ+=a[i];
+        if(summ>summmax){summmax=summ;}
+    }
+    cout << summmax;
+    return 0;
+}
