9f7e1235db1199dbe53e2a4e2b67f340eca470fc
Mot101 <56612863+Mot101@users.noreply.github.com>
2020-06-05 12:38:55 +0300

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+int point=0,c=0,check=0,count=-1;
+string d;
+cin >> d;
+for (int i=0; i<d.length(); i++){
+     if(d[i]!='.' && d[i]!='0' && d[i]!='1' && d[i]!='2' && d[i]!='3' && d[i]!='4' && d[i]!='5' && d[i]!='6' && d[i]!='7' && d[i]!='8' && d[i]!='9'){
+        check=-1;
+     }
+}
+if (check==0){
+    for(int i = 0;i<d.length();i++){
+    if (d[i]!='.'){
+        if(count==-1){
+                count=d[i]-48;
+            }
+        else{
+            count=count*10+d[i]-48;
+        }
+    }
+    else {
+        if( count >= 0 && count<=255){
+                count=-1;
+        }
+        else{
+                d=-1;
+                break;
+        }
+            point++;
+    }
+
+}
+
+if(count>=0 && count<=255 && point== 3){
+        cout << "true";
+    }
+    else{
+            cout << "false" ;
+    }
+}
+}
